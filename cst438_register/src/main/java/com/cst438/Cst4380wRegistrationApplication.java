20fb34a6d5b1ff7683de0f320b2233cd2df14afd
folsterA <folsteraustin@gmail.com>
2023-02-20 17:05:33 -0800

    initial commit

--- cst438_register/src/main/java/com/cst438/Cst4380wRegistrationApplication.java
+++ cst438_register/src/main/java/com/cst438/Cst4380wRegistrationApplication.java
@@ -1,5 +1,5 @@
 package com.cst438;
-
+//
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
